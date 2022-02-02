d2c53c41606ea5cf40ac632e242c1ba8411d7b74
Tobias Köhler <t.koehler@fz-juelich.de>
2022-02-02 20:55:38 +0100

    Add ViewController UserDefaults.standard

--- Todolist/Controllers/ToDoListViewController.swift
+++ Todolist/Controllers/ToDoListViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in:
+                                                        .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
