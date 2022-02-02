d29b3bb4228bd87a7b79f2f82089ed8ddfd79808
Tobias Köhler <t.koehler@fz-juelich.de>
2022-02-02 19:21:22 +0100

    Update folder structure

---
+++ Todolist/Controllers/ToDoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by Tobias Köhler on 02.02.22.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Todolist/Data Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  Todolist
+//
+//  Created by Tobias Köhler on 02.02.22.
+//
+
+import Foundation
