e75cadcfef7b1347e3805405bc50617dd995c2be
Tobias Köhler <t.koehler@fz-juelich.de>
2022-02-02 19:03:47 +0100

    Initial commit

---
+++ Todolist/Todolist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by Tobias Köhler on 02.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
